66f6cb43600f4511930fcf9fc51ae79e918848f1
dadamu4 <bessiadamu@gmail.com>
2020-01-21 15:46:33 -0500

    second commit

--- src/containers/Friends/components/SearchFriends/index.js
+++ src/containers/Friends/components/SearchFriends/index.js
@@ -1,1 +1,27 @@
 // allows the user to search for friends
+
+import React, { useState } from 'react'
+
+
+const SearchFriends = () => {
+    // use the state hook to keep track of the current person the user is typing
+    const [searchFriend, setSearchFriend] = useState('')
+    
+
+    return (
+        <div>
+        
+            <input type="text" placeholder="Search for a friend!" value={searchFriend} onChange={e => setFriend(e.target.value)} />
+            
+
+        </div> 
+    )
+
+
+}
+
+
+
+export default SearchFriends
+
+
