3d1d71bbf8a2ef2100160a10e1d3b8b3977bda72
dadamu4 <bessiadamu@gmail.com>
2020-01-24 11:31:04 -0500

    m

--- src/containers/PositiveMessages/components/ReadSendMessages/index.js
+++ src/containers/PositiveMessages/components/ReadSendMessages/index.js
@@ -1,14 +1,16 @@
 // displays the list of messages the user has recieved from their friends
 
-import React from 'react'
-import { useQuery } from '@apollo/react-hooks'
-import { GET_MESSAGES } from './graphql'
+import React, { useState } from 'react'
+import { useQuery, useMutation } from '@apollo/react-hooks'
+import { GET_MESSAGES, SEND_MESSAGE } from './graphql'
+import Message from './Message'
 
-const ReadMessages = () => {
+const Messages = () => {
   const { loading: getMessagesLoading, error: getMessagesError, data: getMessagesData } = useQuery(GET_MESSAGES)
 
+  const [message, setMessage] = useState('')
   const [sendMessage, { error, loading }] = useMutation(SEND_MESSAGE, {
-    variables: { txtInput: entry },
+    variables: { input: message },
     refetchQueries: () => [{ query: GET_MESSAGES }],
   })
 
@@ -18,16 +20,17 @@ const ReadMessages = () => {
 
   return (
     <div>
+
+      <input value={message} onChange={e => setMessage(e.target.value)} />
+      {loading ? 'Loading...' : <button type="button" onClick={sendMessage}>Send</button>}
+      {error && 'Error'}
+
       {getMessagesData.viewer.messagesReceived.map(el => (
         <p key={el.id}>{`${el.sender.firstName} ${el.sender.lastName}: ${el.content}`}</p>
       ))}
-
-      {/* {getMessagesData.user.map(el => (
-        <p key={el.id}>{`${el.firstName} ${el.lastName}`}</p>
-      ))} */}
 
     </div>
   )
 }
 
-export default ReadMessages
+export default Messages
